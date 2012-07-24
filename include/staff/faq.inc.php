210504b537b80dceebc522e73dac7b9877c0a2ff
soif <wxopwx@gmail.com>
2012-07-24 16:40:07 +0200

    add missing CSRF token in the html form

--- include/staff/faq.inc.php
+++ include/staff/faq.inc.php
@@ -23,6 +23,7 @@ if($faq){
 $info=Format::htmlchars(($errors && $_POST)?$_POST:$info);
 ?>
 <form action="faq.php?<?php echo $qstr; ?>" method="post" id="save" enctype="multipart/form-data">
+ <?php csrf_token(); ?>
  <input type="hidden" name="do" value="<?php echo $action; ?>">
  <input type="hidden" name="a" value="<?php echo Format::htmlchars($_REQUEST['a']); ?>">
  <input type="hidden" name="id" value="<?php echo $info['id']; ?>">
