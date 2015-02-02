11ef04083294cf3433727403e728c806a12a9e7d
Jared Hancock <gravydish@gmail.com>
2015-02-02 12:17:39 -0600

    Revert "Disable auto-responses on staff created tickets"

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -1923,7 +1923,7 @@ class Ticket {
      *
      *  $autorespond and $alertstaff overrides config settings...
      */
-    function create(&$vars, &$errors, $origin, $autorespond=true,
+    function create($vars, &$errors, $origin, $autorespond=true,
             $alertstaff=true) {
         global $ost, $cfg, $thisclient, $_FILES;
 
@@ -2221,16 +2221,10 @@ class Ticket {
         }
 
         $ticket->reload();
-        $dept = $ticket->getDept();
 
-        // See if we need to skip auto-response.
-        $autorespond = isset($create_vars['autorespond'])
-            ? $create_vars['autorespond'] : true;
-
-        if (!$autorespond
+        if(!$cfg->notifyONNewStaffTicket()
                 || !isset($vars['alertuser'])
-                || !$dept->autoRespONNewTicket()
-                || !$cfg->notifyONNewStaffTicket())
+                || !($dept=$ticket->getDept()))
             return $ticket; //No alerts.
 
         //Send Notice to user --- if requested AND enabled!!
